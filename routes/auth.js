dc354077e2f8a1b3eaf65cb5318e86b317921215
Yusuf Bukhari <yusufbukhari@gmail.com>
2024-11-23 17:07:06 +0000

    added some validations to register

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const User = require('../models/User')
 const router = express.Router()
+const {registerValidation} = require('../validations/validation')
 
 router.post('/register', async(req,res)=>{
+
+    const {error} = registerValidation(req.body)
+    if (error){
+        return res.status(400).send({message:error['details'][0]['message']})
+    }
     
     const user = new User({
         username:req.body.username,
