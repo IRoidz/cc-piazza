1befbaef690f3dece94e63f5eaf07b7e2ef86b5b
Yusuf Bukhari <yusufbukhari@gmail.com>
2024-11-23 18:22:10 +0000

    adding password hash

--- routes/auth.js
+++ routes/auth.js
@@ -2,23 +2,41 @@ const express = require('express')
 const User = require('../models/User')
 const router = express.Router()
 const {registerValidation} = require('../validations/validation')
+const bcryptjs = require('bcryptjs')
 
 router.post('/register', async(req,res)=>{
 
+    //Validation to check user input
     const {error} = registerValidation(req.body)
     if (error){
         return res.status(400).send({message:error['details'][0]['message']})
     }
+
+    //Validation to check is user already exists
+    const userExsists = await User.findOne({email:req.body.email})
+    if (userExsists){
+        return res.status(400).send({message:'User already exists'})
+    }
+
+    const salt = await bcryptjs.genSalt(5)
+    const hashedPassword = await bcryptjs.hash(req.body.password,salt)
     
+    //code to insert user data
     const user = new User({
         username:req.body.username,
         email:req.body.email,
-        password:req.body.password
+        password:hashedPassword
     })
 
     try{
         const usertoadd = user.save()
-        res.send(usertoadd)
+        res.send({
+            message: "User registered successfully!",
+            user: {
+                username: user.username,
+                email: user.email,
+            },
+        })
     }catch(err){
         res.status(400).send({message:err})
     }
--- validations/validation.js
+++ validations/validation.js
@@ -2,7 +2,7 @@ const joi = require('joi')
 
 const registerValidation = (data) =>{
     const schemaValidation = joi.object({
-        username:joi.string().required().min(3).max(40),
+        username:joi.string().required().min(2).max(40),
         email:joi.string().required().min(6).max(256).email(),
         password:joi.string().required().min(6).max(1024)
     })
