3cfdd3bb211282a4e3ced8c09a7f56fcb60dab45
Yusuf Bukhari <yusufbukhari@gmail.com>
2024-11-23 02:03:35 +0000

    phase a

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express')
-const app = express()
-
 const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-require('dotenv')
+require('dotenv/config')
+
+const app = express()
 
 app.get('/', (req,res)=>{
     res.send("you're in homepage")
 })
+
+mongoose.connect(process.env.DB_CONNECTOR).then(()=>{
+    console.log('Your mongoDB connector is on...')
+})
+
 
 app.listen(3000, ()=>{
     console.log('up and running')
