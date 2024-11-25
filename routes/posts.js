7ff5bcb25fbdad2741b750db8ac09f6c2b59f54d
Yusuf Bukhari <yusufbukhari@gmail.com>
2024-11-25 20:17:15 +0000

    allow users to filter between topics

--- routes/posts.js
+++ routes/posts.js
@@ -3,21 +3,47 @@ const router = express.Router()
 
 const Post = require('../models/Post')
 
+const verify = require('../verifyToken')
+
+const {postValidation} = require('../validations/validation')
+
+// router.get('/', async(req,res)=>{
+//     try{
+//         const posts = await Post.find()
+//         res.send(posts)
+//     }catch(err){
+//         res.status(400).send({message:err})
+//     }
+// })
+
 router.get('/', async(req,res)=>{
+    const topics = req.query.topics
+    if (!topics) {
+        return res.status(400).send({message: err.message})
+    }
+
+    const topicList = topics.split(',')
     try{
-        const posts = await Post.find()
+        const posts = await Post.find({ topic: { $in: topicList } })
+        //const posts = await Post.find({ topic: { $regex: new RegExp(`^${topic}$`, 'i') } });
         res.send(posts)
     }catch(err){
         res.status(400).send({message:err})
     }
 })
 
-router.post('/', async(req,res)=>{
+router.post('/', verify, async(req,res)=>{
+
+    const {error} = postValidation(req.body)
+    if (error){
+       return res.status(400).send({message:error['details'][0]['message']})
+   }
+
     const post = Post({
         title: req.body.title,
         topic: req.body.topic,
         body: req.body.body,
-        owner: req.body.owner,
+        owner: req.user._id
     })
     try{
         const savedPost = await post.save()
--- validations/validation.js
+++ validations/validation.js
@@ -2,20 +2,32 @@ const joi = require('joi')
 
 const registerValidation = (data) =>{
     const schemaValidation = joi.object({
-        username:joi.string().required().min(2).max(40),
-        email:joi.string().required().min(6).max(256).email(),
-        password:joi.string().required().min(6).max(1024)
+        username: joi.string().required().min(2).max(40),
+        email: joi.string().required().min(6).max(256).email(),
+        password: joi.string().required().min(6).max(1024)
     })
     return schemaValidation.validate(data)
 }
 
 const loginValidation = (data) =>{
     const schemaValidation = joi.object({
-        email:joi.string().required().min(6).max(256).email(),
-        password:joi.string().required().min(6).max(1024)
+        email: joi.string().required().min(6).max(256).email(),
+        password: joi.string().required().min(6).max(1024)
+    })
+    return schemaValidation.validate(data)
+}
+
+const postValidation = (data) =>{
+    const schemaValidation = joi.object({
+        title: joi.string().required().min(1).max(1000),
+        topic: joi.array().items(joi.string()
+        .valid('Politics', 'Health', 'Sport', 'Tech'))
+        .required(),
+        body: joi.string().required().max(3000),
     })
     return schemaValidation.validate(data)
 }
 
 module.exports.registerValidation = registerValidation
 module.exports.loginValidation = loginValidation
+module.exports.postValidation = postValidation
